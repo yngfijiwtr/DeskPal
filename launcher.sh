35a5bb2626f0e1d201250b3ddd9924f1fa18b1c5
KoloKush <167939409+KoloKush@users.noreply.github.com>
2024-05-15 23:48:06 -0700

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -2,7 +2,7 @@
 #launcher.sh
 # navigate to home directory then to this directory, then execute python, then back home
 cd /
-cd home/kolokush/
-python3 Main/MainProgramV1.py
+cd home/pi/
+python3 Main/MainProgramV3.py
 cd /
 
